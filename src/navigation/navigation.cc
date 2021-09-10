cf501d250417989d6a6338fb1f9e1e56e92ec0c7
saikm200022 <44325970+saikm200022@users.noreply.github.com>
2021-09-10 13:58:35 -0500

    1D TOC Done

--- src/navigation/navigation.cc
+++ src/navigation/navigation.cc
@@ -83,8 +83,6 @@ void Navigation::UpdateLocation(const Eigen::Vector2f& loc, float angle) {
   robot_angle_ = angle;
 }
 
-
-
 void Navigation::UpdateOdometry(const Vector2f& loc,
                                 float angle,
                                 const Vector2f& vel,
@@ -108,6 +106,43 @@ void Navigation::ObservePointCloud(const vector<Vector2f>& cloud,
   point_cloud_ = cloud;                                     
 }
 
+float dist = 10.0;
+
+float Navigation::Simple1DTOC(Vector2f point)
+{
+  float v_max = 1.0;
+  float max_a = 0.4;
+  float max_d = 0.4;
+  float x3 = pow(v_max,2) / max_d;
+  Vector2f base_link;
+  base_link[0] = 0;
+  base_link[1] = 0;
+  float distance_left  = abs((point - base_link).norm());
+
+  // accelerate towards vmax
+  if (robot_vel_[0] < v_max && distance_left > x3)
+  {
+    printf("a\n");
+    float new_v = robot_vel_[0] + max_a * 1/20;
+    return std::min(new_v, v_max);
+  }
+  
+  // Cruise
+  if (robot_vel_[0] == v_max && distance_left > x3) 
+  {
+    printf("\tc\n");
+    return v_max;
+  }
+
+  printf("\t\td\n");
+  
+  // Stop
+  float new_v = robot_vel_[0] - max_d * 1/20;
+  float zero = 0.0;
+  return std::max(new_v, zero);
+}
+
+
 void Navigation::Run() {
   // This function gets called 20 times a second to form the control loop.
   
@@ -122,14 +157,16 @@ void Navigation::Run() {
   // Feel free to make helper functions to structure the control appropriately.
   
   // The latest observed point cloud is accessible via "point_cloud_"
+  visualization::DrawPoint(Vector2f(dist, 0),0x4287f5,local_viz_msg_);
 
   // Eventually, you will have to set the control values to issue drive commands:
   drive_msg_.curvature = 0;
-  drive_msg_.velocity = 1.0;
+  drive_msg_.velocity = Simple1DTOC(Vector2f(dist, 0));
+  dist -= drive_msg_.velocity * 1/20;
 
-  for (auto point : point_cloud_) {
-    visualization::DrawPoint(point,0x4287f5,local_viz_msg_);
-  }
+  // for (auto point : point_cloud_) {
+  //   visualization::DrawPoint(point,0x4287f5,local_viz_msg_);
+  // }
 
   // Add timestamps to all messages.
   local_viz_msg_.header.stamp = ros::Time::now();
@@ -141,73 +178,36 @@ void Navigation::Run() {
   drive_pub_.publish(drive_msg_);
 }
 
-// float Navigation::Simple1DTOC(Vector2f point)
-// {
-//   float distance_constraint = v_max / max_d;
-//   float_distance_left  = abs(point)
-//   if (robot_vel_ < v_max && distance_left)
-//     return robot_vel_ + max_a;
-  
-//   else if (robot_vel_ == v_max && distance_left)
-//     return robot_vel;
-  
-//   return robot_vel - max_d;
+// float Navigation::GetMaxDistance(float theta, Vector2f point) { 
+//   float radius = width / tan(theta));
+//   Vector2f CoT(0,radius);
+
+//   float max_radius = sqrt(pow((radius+width)/2.0,2) + pow(length - (length - wheelbase)/2.0, 2));
+//   float min_radius = radius - width/2.0;
+
+//   float point_radius = abs(point - CoT);
+
+//   if (point_radius >= min_radius && point_radius <= max_radius) {
+//     // Collision with this point
+//     float inner_corner_radius = sqrt(pow(min_radius,2) + pow(length - (length - wheelbase)/2.0,2));
+//     Vector2f collision_point;
+//     if (point_radius >= min_radius && point_radius <= inner_corner_radius) {
+//       // collision with side
+//       collision_point[1] = width/2.0;
+//       collision_point[0] = sqrt(pow(point_radius,2) - pow(min_radius,2));
+
+//     } else {
+//       // collision with front
+//       collision_point[1] = sqrt(pow(point_radius,2)- pow(length - (length - wheelbase)/2),2) - radius;
+//       collision_point[0] = length - (length - wheelbase)/2
+//     }
+
+//     float collision_angle = 2 * asin(abs(point - collision_point) / (2 * point_radius));
+//     float max_distance = point_radius * collision_angle;
+//     return max_distance;
+
+//   }
 // }
-
-bool Navigation::PointCollidesWithArc(float theta, Eigen::Vector2f point) {
-  double width = WIDTH + SAFETY_MARGIN;
-  double length = LENGTH + SAFETY_MARGIN;
-  double wheelbase = WHEELBASE;
-
-  double radius = width / tan(theta);
-  Vector2f CoT(0,radius);
-
-  double max_radius = sqrt(pow((radius+width)/2.0,2) + pow(length - (length - wheelbase)/2.0, 2));
-  double min_radius = radius - width/2.0;
-
-  auto point_dis = point - CoT;
-  double point_radius = abs(point_dis.norm());
-
-  return (point_radius >= min_radius && point_radius <= max_radius);
-}
-
-// Assumes collision is already checked
-float Navigation::GetMaxDistance(float theta, Eigen::Vector2f point) { 
-  double width = WIDTH + SAFETY_MARGIN;
-  double length = LENGTH + SAFETY_MARGIN;
-  double wheelbase = WHEELBASE;
-
-  double radius = width / tan(theta);
-  Vector2f CoT(0,radius);
-
-  double max_radius = sqrt(pow((radius+width)/2.0,2) + pow(length - (length - wheelbase)/2.0, 2));
-  double min_radius = radius - width/2.0;
-
-  auto point_dis = point - CoT;
-  double point_radius = abs(point_dis.norm());
-
-  if (point_radius >= min_radius && point_radius <= max_radius) {
-    // Collision with this point
-    double inner_corner_radius = sqrt(pow(min_radius,2) + pow(length - (length - wheelbase)/2.0,2));
-    Vector2f collision_point;
-    if (point_radius >= min_radius && point_radius <= inner_corner_radius) {
-      // collision with side
-      collision_point[1] = width/2.0;
-      collision_point[0] = sqrt(pow(point_radius,2) - pow(min_radius,2));
-
-    } else {
-      // collision with front
-      collision_point[1] = sqrt(pow(point_radius,2) - pow(length - (length - wheelbase)/2,2)) - radius;
-      collision_point[0] = length - (length - wheelbase)/2;
-    }
-
-    double collision_angle = 2 * asin(abs((point - collision_point).norm()) / (2 * point_radius));
-    double max_distance = point_radius * collision_angle;
-    return max_distance;
-
-  }
-  return -1;
-}
 
 
 }  // namespace navigation
